3c68b0ae5badf062a2c2f5b86117a9488ed1dae5
TiThuthur <thurthur.petit.py@gmail.com>
2024-04-27 21:49:45 +0200

    starting project

---
+++ README.md
@@ -0,0 +1,5 @@
+# booking_hotels
+
+tutorial sur la création d'une app sur la réservation d'hotels.
+
+https://drissas.com/flutter-tutorial-francais/
---
+++ android/app/src/main/kotlin/com/booking_hotels/booking_hotels/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.booking_hotels.booking_hotels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
